1980580433077660495834e6db92750374337bfd
Tyler Firth <tyfirth@gmail.com>
2020-05-29 19:38:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -97,10 +97,10 @@ end
   def winner
        WIN_COMBINATIONS.detect do |win_combo|
      if (@board[win_combo[0]]) == "X" && (@board[win_combo[1]]) == "X" && (@board[win_combo[2]]) == "X"
-       return win_combo
+       return "X"
      elsif
         (@board[win_combo[0]]) == "O" && (@board[win_combo[1]]) == "O" && (@board[win_combo[2]]) == "O"
-        return win_combo
+        return "O"
   end
   
 end
