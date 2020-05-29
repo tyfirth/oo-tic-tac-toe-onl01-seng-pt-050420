e66b2141ae4e99a00afd0c12a6084e05b33ac352
Tyler Firth <tyfirth@gmail.com>
2020-05-29 20:18:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,7 +3,7 @@ class TicTacToe
   def initialize(board = nil)
     @board = board || Array.new(9, " ")
   end
-end
+
   
   WIN_COMBINATIONS = [
     [0,1,2],
@@ -119,3 +119,4 @@ end
   end
   end
   
+end
