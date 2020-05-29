c73c5f98869fd6136c6384f44649a93b4e665d2a
Tyler Firth <tyfirth@gmail.com>
2020-05-29 18:37:53 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -44,7 +44,7 @@ end
     input = gets.strip
     index = input_to_index(input)
     token = current_player
-      if valid_move(index)
+      if valid_move?(index)
         move(index, token)
         display_board
       else 
