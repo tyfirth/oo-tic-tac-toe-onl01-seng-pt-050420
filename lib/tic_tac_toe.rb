10b5fe47b5f70c07235b0e452292e8f1763876de
Tyler Firth <tyfirth@gmail.com>
2020-05-29 18:38:41 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -43,9 +43,9 @@ end
     puts "Enter 1 - 9"
     input = gets.strip
     index = input_to_index(input)
-    token = current_player
+    char = current_player
       if valid_move?(index)
-        move(index, token)
+        move(index, char)
         display_board
       else 
         turn
